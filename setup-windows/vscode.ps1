228704b897cd8e84a93dc638fe4c4c8b9209ff07
Greg Poole <m4dm4n@gmail.com>
2019-09-01 18:52:46 +1000

    Automate vscode extension install for Mac + Windows, change js settings
    - Added a list of vscode extensions to be installed and set up
    Windows and Mac scripts to use it
    - No longer using prettier-eslint, so remove settings related to that

--- setup-windows/vscode.ps1
+++ setup-windows/vscode.ps1
@@ -11,7 +11,6 @@ function Add-Config {
 Add-Config "vscode-settings.json" "settings.json"
 Add-Config "vscode-keybindings.json" "keybindings.json"
 
-code --install-extension ms-vscode.csharp
-code --install-extension ms-vscode.powershell
-code --install-extension vscodevim.vim
-code --install-extension golf1052.base16-generator
+Get-Content (Join-Path $myConfigPath "vscode-extensions.txt") | ForEach-Object {
+  code --install-extension $_
+}
