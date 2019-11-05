091cb56e7698463b637683f396c70887f6c18203
Greg Poole <m4dm4n@gmail.com>
2019-11-05 15:42:49 +1100

    Windows: Chocolatey config in Powershell profile

--- profile.ps1
+++ profile.ps1
@@ -31,3 +31,9 @@ function prompt {
 
   return "> "
 }
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
