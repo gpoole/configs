c29cf648f80e4b73f158713f1cb30adb26dbbde0
Greg Poole <greg.poole@protonmail.com>
2021-12-09 18:43:49 +1100

    Setting up on Mac, no need for vscode stuff anymore

--- windows/util/diff-vscode-extensions.ps1
+++
@@ -1,4 +0,0 @@
-cat shared/vscode-*.txt | sort | unique > $env:TEMP/vscode-proposed.txt
-code --list-extensions | sort > $env:TEMP/vscode-current.txt
-code --diff $env:TEMP/vscode-current.txt $env:TEMP/vscode-proposed.txt
-
